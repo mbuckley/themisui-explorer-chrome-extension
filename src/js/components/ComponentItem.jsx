752535d5e13571c139c6ebbb4920ef4ee9cd2c86
Mike Buckley <mikebuckley.canada@gmail.com>
2016-12-19 21:13:21 -0800

    Fixed events for component item row in popup

--- src/js/components/ComponentItem.jsx
+++ src/js/components/ComponentItem.jsx
@@ -1,17 +1,54 @@
 import styles from '../../styles/component-item.scss';
 
 import React from 'react';
+import classnames from 'classnames';
 import {render} from 'react-dom';
 
-// anchor.addEventListener('mouseenter', showCustomElements);
-// anchor.addEventListener('click', togglePinnedCustomElements);
-// anchor.addEventListener('mouseleave', hideCustomElements);
+//TODO: Add this onclick to a pin icon so it doesn't conflict
+//with the click to see the element docs
+// onClick={this.togglePinnedCustomElements.bind(this)}
 class ComponentItem extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    console.log("componentItem props", props);
+    this.state = { active: false };
+  }
 
+  render () {
+    let classes = classnames(styles.componentItem, {active: this.state.active});
     return (
-      <a target="_blank" className={styles.componentItem}>{this.props.label}</a>
+      <a target="_blank"
+         className={classes}
+         href={'http://themisui-docs.clio.com/' + this.props.label}
+         onMouseEnter={this.showCustomElements.bind(this)}
+         onMouseLeave={this.hideCustomElements.bind(this)}
+      >{this.props.label}</a>
     )
+  }
+
+  // togglePinnedCustomElements(e) {
+  //   this.setState({active: this.state.active ? false : true});
+  //
+  //   if (this.state.active) {
+  //     this.showCustomElements(event);
+  //   } else {
+  //     this.hideCustomElements();
+  //   }
+  // }
+
+  showCustomElements(event) {
+    this.props.port.postMessage({
+      action: 'show-custom-elements',
+      filter: event.target.textContent,
+      pinned: Array.from(document.querySelectorAll('.active')).map(e => e.textContent)
+    });
+  }
+
+  hideCustomElements(event) {
+    this.props.port.postMessage({
+      action: 'hide-custom-elements',
+      pinned: Array.from(document.querySelectorAll('.active')).map(e => e.textContent)
+    });
   }
 }
 
--- src/js/components/Popup.jsx
+++ src/js/components/Popup.jsx
@@ -2,43 +2,14 @@ import styles from '../../styles/popup.scss';
 import React from 'react';
 import {render} from 'react-dom';
 import ComponentItem from './ComponentItem';
-// import {initializePopup} from '../popup.js'
 
-//TODO: Render each ComponentItem into the components-container below
-//TODO: Replace class="collection" with style from "styles" import
+let port;
 
-function showCustomElements(event) {
-  port.postMessage({ action: 'show-custom-elements',
-                     filter: event.target.textContent,
-                     pinned: Array.from(document.querySelectorAll('.active')).map(e => e.textContent)
-                   });
-}
-
-function hideCustomElements(event) {
-  port.postMessage({ action: 'hide-custom-elements',
-                     pinned: Array.from(document.querySelectorAll('.active')).map(e => e.textContent)
-                   });
-}
-
-function togglePinnedCustomElements(event) {
-  this.classList.toggle('active');
-  if (this.classList.contains('active')) {
-    showCustomElements(event);
-  } else {
-    hideCustomElements();
-  }
-}
-
-//TODO: Move into components/ComponentItem.jsx
-function probeThemisUIElements(elements) {
-  elements.forEach(function(element) {
-    element.querySelector('a').href = 'http://themisui-docs.clio.com/' + element.textContent;
-  });
-}
-
-//FIXME: Find out why this isn't firing...inside of export?
-function initializePopup() {
-  let port;
+/**
+ * Initialize first grabs the current tab and connects to the port
+ * and renders when the addListener comes back.
+ */
+function initialize() {
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     port = chrome.tabs.connect(tabs[0].id);
@@ -47,32 +18,6 @@ function initializePopup() {
 
     port.onMessage.addListener(function(response) {
       render(<Popup customElements={response.customElements} />, document.getElementById('popup'));
-
-
-      // let existingElements = document.querySelectorAll('.el');
-      // let addSeparator = (existingElements.length !== 0);
-      //
-      // //TODO: Move into components/Popup.jsx
-      // let containerEle = document.getElementById("components-container");
-      //
-      // response.customElements.forEach(function(el) {
-      //   //TODO: call render of popup here...
-      //
-      //   let anchor = document.createElement('a');
-      //   anchor.target= '_blank';
-      //   anchor.textContent = el;
-      //   anchor.classList.add('collection-item');
-      //   // elementListItemInnerEl.appendChild(anchor);
-      //
-      //   anchor.addEventListener('mouseenter', showCustomElements);
-      //   anchor.addEventListener('click', togglePinnedCustomElements);
-      //   anchor.addEventListener('mouseleave', hideCustomElements);
-      //
-      //   containerEle.appendChild(anchor);
-      // });
-
-      // let elements = document.querySelectorAll('.mdl-list__item');ß
-      // probeThemisUIElements(elements);
     });
   });
 
@@ -84,6 +29,8 @@ function initializePopup() {
 class Popup extends React.Component {
   constructor(props) {
     super(props);
+    console.log("port", port);
+    this.state = { port: port };
   }
 
   render () {
@@ -91,7 +38,7 @@ class Popup extends React.Component {
       <div className="component-list">
         {
           this.props.customElements.map((componentName, index) => {
-            return <ComponentItem key={index} label={componentName} />
+            return <ComponentItem key={index} label={componentName} port={this.state.port} />
           })
         }
       </div>
@@ -99,4 +46,4 @@ class Popup extends React.Component {
   }
 }
 
-initializePopup();
+initialize();
